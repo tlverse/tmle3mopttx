23a393907203cfefe2e81324b56536751a54b047
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-10-04 10:37:24 -0700

    force categorical A

--- R/tmle3_Spec_mopttx_vim.R
+++ R/tmle3_Spec_mopttx_vim.R
@@ -19,10 +19,57 @@ tmle3_Spec_mopttx_vim <- R6Class(
     vals_from_factor = function(x) {
       sort(unique(x))
     },
+    make_tmle_task = function(data, node_list, ...) {
+       # bound Y if continuous
+      Y_node <- node_list$Y
+      Y_vals <- unlist(data[, Y_node, with = FALSE])
+      Y_variable_type <- variable_type(x = Y_vals)
+      if (Y_variable_type$type == "continuous") {
+        min_Y <- min(Y_vals)
+        max_Y <- max(Y_vals)
+        range <- max_Y - min_Y
+        lower <- min_Y # - 0.1 * range
+        upper <- max_Y # + 0.1 * range
+        Y_variable_type <- variable_type(
+          type = "continuous",
+          bounds = c(lower, upper)
+        )
+      }
 
+       # todo: export and use sl3:::get_levels
+      A_node <- node_list$A
+      A_vals <- unlist(data[, A_node, with = FALSE])
+      if (is.factor(A_vals)) {
+        A_levels <- sort(unique(A_vals))
+        A_levels <- factor(A_levels, A_levels)
+      } else {
+        A_levels <- sort(unique(A_vals))
+      }
+      A_variable_type <- variable_type(
+        type = "categorical",
+        levels = A_levels
+      )
+
+      # make tmle_task
+      npsem <- list(
+        define_node("W", node_list$W),
+        define_node("A", node_list$A, c("W"), A_variable_type),
+        define_node("Y", node_list$Y, c("A", "W"), Y_variable_type)
+      )
+
+      if(!is.null(node_list$id)){
+        tmle_task <- tmle3_Task$new(data, npsem = npsem, id=node_list$id, ...)
+      } else {
+        tmle_task <- tmle3_Task$new(data, npsem = npsem, ...)
+      }
+
+      return(tmle_task)
+
+    },
     make_updater = function() {
       updater <- tmle3_cv_Update$new()
     },
+    
 
     make_params = function(tmle_task, likelihood) {
 
