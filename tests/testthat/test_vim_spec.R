ce3645ec62f1db857d7bc500d8da2f0db4bb1e3b
Ivana <imalenica.stat@gmail.com>
2019-05-20 16:31:17 -0700

    Add vim test

--- tests/testthat/test_vim_spec.R
+++ tests/testthat/test_vim_spec.R
@@ -1,54 +1,53 @@
-# context("Test categorical rule Variable Importance Measure")
+context("Test categorical rule Variable Importance Measure")
 
-#library(sl3)
-#library(data.table)
-#library(tmle3mopttx)
-#library(tmle3)
+library(sl3)
+library(data.table)
+library(tmle3mopttx)
+library(tmle3)
 
-# set.seed(1234)
+set.seed(1234)
 
-# data("data_cat")
-# data <- data_cat
+data("data_cat_vim")
+data <- data_cat_vim
 
-# xgboost_100 <- Lrnr_xgboost$new(nrounds = 100)
-# xgboost_500 <- Lrnr_xgboost$new(nrounds = 500)
-# lrn1 <- Lrnr_mean$new()
-# lrn2 <- Lrnr_glm_fast$new()
+xgboost_100 <- Lrnr_xgboost$new(nrounds = 100)
+xgboost_500 <- Lrnr_xgboost$new(nrounds = 500)
+lrn1 <- Lrnr_mean$new()
+lrn2 <- Lrnr_glm_fast$new()
 
-# Q_learner <- Lrnr_sl$new(
-#  learners = list(xgboost_100, xgboost_500, lrn1, lrn2),
-#  metalearner = Lrnr_nnls$new()
-# )
+Q_learner <- Lrnr_sl$new(
+  learners = list(xgboost_100, xgboost_500, lrn1, lrn2),
+  metalearner = Lrnr_nnls$new()
+ )
 
-# mn_metalearner <- make_learner(Lrnr_solnp,
-#  loss_function = loss_loglik_multinomial,
-#  learner_function = metalearner_linear_multinomial
-# )
-# g_learner <- make_learner(Lrnr_sl, list(xgboost_100, xgboost_500, lrn1), mn_metalearner)
+mn_metalearner <- make_learner(Lrnr_solnp,
+  loss_function = loss_loglik_multinomial,
+  learner_function = metalearner_linear_multinomial
+)
+g_learner <- make_learner(Lrnr_sl, list(xgboost_100, xgboost_500, lrn1), mn_metalearner)
 
 # Define the Blip learner, which is a multivariate learner:
-# learners <- list(xgboost_100, xgboost_500, lrn1, lrn2)
-# b_learner <- create_mv_learners(learners = learners)
+learners <- list(xgboost_100, xgboost_500, lrn1, lrn2)
+b_learner <- create_mv_learners(learners = learners)
 
-# learner_list <- list(Y = Q_learner, A = g_learner, B = b_learner)
+learner_list <- list(Y = Q_learner, A = g_learner, B = b_learner)
 
 # Define nodes:
-# node_list <- list(W = c("W1", "W2", "W3", "W4"), A = "A", Y = "Y")
+node_list <- list(W = c("W2", "W3", "W4"), A = c("A","W1"), Y = "Y")
 
-# test_that("Categorical rule, Variable Importance", {
-#  tmle_spec <- tmle3_mopttx_vim(
-#    V = node_list$W, learners = learner_list, type = "blip2",
-#    contrast = "multiplicative",
-#    maximize = FALSE,
-#    method = "SL", complex = TRUE, realistic = FALSE
-#  )
+test_that("Categorical rule, Variable Importance", {
+  tmle_spec <- tmle3_mopttx_vim(
+    V = "W3", learners = learner_list, type = "blip2",
+    contrast = "multiplicative",
+    maximize = FALSE,
+    method = "SL", complex = TRUE, realistic = FALSE)
 
-#  vim_results <- tmle3_vim(tmle_spec, data,
-#    node_list = node_list, learner_list,
-#    adjust_for_other_A = TRUE
-#  )
-# expect_equal(vim_results$tmle_est[1], -0.318628, tolerance = 0.2)
-# })
+  vim_results <- tmle3_vim(tmle_spec, data, 
+                           node_list = node_list, learner_list,
+                           adjust_for_other_A = TRUE)
+
+  expect_equal(vim_results$tmle_est[1], -0.2397367, tolerance = 0.2)
+})
 
 # tmle_task <- tmle_spec_opttx$make_tmle_task(data, node_list)
 # g_task <- tmle_task$get_regression_task("A")
