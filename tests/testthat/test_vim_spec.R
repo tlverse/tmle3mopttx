b8db305676e39b543379214dafb49dc4739570b9
Ivana <imalenica.stat@gmail.com>
2021-03-29 23:53:26 -0700

    faster test

--- tests/testthat/test_vim_spec.R
+++ tests/testthat/test_vim_spec.R
@@ -14,9 +14,10 @@ xgboost_100 <- Lrnr_xgboost$new(nrounds = 100)
 xgboost_500 <- Lrnr_xgboost$new(nrounds = 500)
 lrn1 <- Lrnr_mean$new()
 lrn2 <- Lrnr_glm_fast$new()
+lrn3 <- Lrnr_glmnet$new()
 
 Q_learner <- Lrnr_sl$new(
-  learners = list(xgboost_100, lrn1, lrn2),
+  learners = list(lrn1, lrn2, lrn3),
   metalearner = Lrnr_nnls$new()
 )
 
@@ -24,10 +25,10 @@ mn_metalearner <- make_learner(Lrnr_solnp,
   loss_function = loss_loglik_multinomial,
   learner_function = metalearner_linear_multinomial
 )
-g_learner <- make_learner(Lrnr_sl, list(xgboost_100, lrn1), mn_metalearner)
+g_learner <- make_learner(Lrnr_sl, list(lrn3, xgboost_100, lrn1), mn_metalearner)
 
 # Define the Blip learner, which is a multivariate learner:
-learners <- list(xgboost_100, lrn1, lrn2)
+learners <- list(lrn1, lrn2)
 b_learner <- create_mv_learners(learners = learners)
 
 learner_list <- list(Y = Q_learner, A = g_learner, B = b_learner)
